cbec0232ba8a56d9c7fc3264f6a5898f76efe203
Porrapat Petchdamrongskul <porrapat.cpe20@gmail.com>
2018-08-18 22:33:31 +0700

    Add more

---
+++ divsum.php
@@ -0,0 +1,22 @@
+<?php
+$stdin = fopen('php://stdin', 'r');
+// Get first line
+$number_test_case = intval(fgets($stdin));
+
+for($test_case_i = 0; $test_case_i < $number_test_case; $test_case_i++)
+{
+    $n = intval(fgets($stdin));    // Get third line
+
+    $sum = 0;
+    for($i = 1; $i< $n; $i++)
+    {
+        if($n % $i == 0)
+        {
+            $sum += $i;
+        }
+    }
+    echo $sum;
+    echo "\n";
+}
+
+fclose($stdin);
--- example_1.php
+++ example_1.php
@@ -1,36 +1,31 @@
 <?php
+
+// To use it on unix system with PHP command line
+// php -v
+// cat example_1.txt | php example_1.php
+
+// Use PHP standard input.
 $stdin = fopen('php://stdin', 'r');
 
+// Get first line
 $number_test_case = intval(fgets($stdin));
-
-// echo $number_test_case;
 
 for($i = 0; $i < $number_test_case; $i++)
 {
-    $n = intval(fgets($stdin));
-    $line_m = fgets($stdin);
-
-    // echo $line_m;
+    $n = intval(fgets($stdin)); // Get second line
+    $line_m = fgets($stdin);    // Get third line
 
     $arr_m = explode(" ", $line_m);
 
-    // for
     $sum = 0;
     for($j = 0; $j < $n; $j++)
     {
         $sum += intval($arr_m[$j]);
     }
 
+    // Print output each line.
     echo "Case #".$i.": ".$sum;
     echo "\n";
-
-
-
-    // echo $n;
-    // echo "\n";
-    // echo "\n";
-    // echo $i;
-    // echo "\n";
 }
 
 fclose($stdin);
