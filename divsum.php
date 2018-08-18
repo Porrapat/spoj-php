a7b2780d6df3c788662aec509eed8b04f6cf629e
Porrapat Petchdamrongskul <porrapat.cpe20@gmail.com>
2018-08-18 23:02:18 +0700

    OK now

--- divsum.php
+++ divsum.php
@@ -8,13 +8,37 @@ for($test_case_i = 0; $test_case_i < $number_test_case; $test_case_i++)
     $n = intval(fgets($stdin));    // Get next line
 
     $sum = 0;
-    for($i = 1; $i< $n; $i++)
+    $v = [];
+    $t = 0;
+    for ($i = 1; $i <= (int)sqrt($n); $i++)
     {
-        if($n % $i == 0)
+        if ($n % $i == 0)
         {
-            $sum += $i;
+            // check if divisors are equal
+            if ((int)$n / $i == $i) 
+            {
+            }
+            else
+            {
+                $sum += $i;
+ 
+                // push the second 
+                // divisor in the vector
+                $v[$t++] = (int)$n / $i;
+            }
         }
     }
+ 
+    // The vector will be
+    // printed in reverse
+    for ($i = count($v) - 1; $i >= 0; $i--)
+    {
+        if($v[$i] != $n)
+        {
+            $sum += $v[$i];
+        }
+    }
+
     echo $sum;
     echo "\n";
 }
