74891bea78c38217c292969fb8b97d48873671a5
Porrapat Petchdamrongskul <porrapat.cpe20@gmail.com>
2018-08-22 10:59:04 +0700

    Add more I already fixed this.

--- divsum.php
+++ divsum.php
@@ -10,51 +10,63 @@ for($test_case_i = 0; $test_case_i < $number_test_case; $test_case_i++)
 {
     $n = intval(fgets($stdin));    // Get next line
 
-    $sum = 0;
-    $v = [];
-    $t = 0;
-    $sqrt_n = (int)sqrt($n);
-    for ($i = 1; $i <= $sqrt_n; $i++)
+    if($n == 1)
     {
-        if ($n % $i == 0)
+        echo 0;
+        if($test_case_i < $number_test_case-1)
         {
-            // check if divisors are equal
-            if ((int)$n / $i == $i) 
-            {
-            }
-            else
-            {
-                $sum += $i;
- 
-                // push the second 
-                // divisor in the vector
-                $v[$t++] = (int)$n / $i;
-            }
+            echo "\n";
         }
     }
- 
-    // The vector will be
-    // printed in reverse
-    for ($i = count($v) - 1; $i >= 0; $i--)
+    else
     {
-        if($v[$i] != $n)
+        $sum = 0;
+        $v = [];
+        $t = 0;
+        $sqrt_n = (int)sqrt($n);
+        for ($i = 1; $i <= $sqrt_n; $i++)
         {
-            $sum += $v[$i];
+            if ($n % $i === 0)
+            {
+                // check if divisors are equal
+                if ((int)($n / $i) === $i) 
+                {
+                    $sum += $i;
+                }
+                else
+                {
+                    $sum += $i;
+    
+                    // push the second 
+                    // divisor in the vector
+                    $v[$t++] = (int)($n / $i);
+                }
+            }
         }
-    }
+    
+        // The vector will be
+        // printed in reverse
+        for ($i = count($v) - 1; $i >= 0; $i--)
+        {
+            if($v[$i] !== $n)
+            {
+                $sum += $v[$i];
+            }
+        }
 
-    echo $sum;
-    if($test_case_i < $number_test_case-1)
-    {
-        echo "\n";
+        echo $sum;
+        if($test_case_i < $number_test_case-1)
+        {
+            echo "\n";
+        }
     }
 }
 
-function convert($size)
-{
-    $unit=array('b','kb','mb','gb','tb','pb');
-    return @round($size/pow(1024,($i=floor(log($size,1024)))),2).' '.$unit[$i];
-}
+// function convert($size)
+// {
+//     $unit=array('b','kb','mb','gb','tb','pb');
+//     return @round($size/pow(1024,($i=floor(log($size,1024)))),2).' '.$unit[$i];
+// }
 
 // echo "\n";
 // echo convert(memory_get_usage())."\n";
